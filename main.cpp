3e4d7b7d02bb01fb0b66a30ba76048027cfc939c
ahyan m <ahyanm24@gmail.com>
2025-02-14 23:58:04 -0800

    Included functions to validate user name as non-empty and user ID value as 8-digit int

--- main.cpp
+++ main.cpp
@@ -10,18 +10,78 @@ Using I/O manipulation to display catalog. Using overloaded methods to search fo
 
 //Ensure that system integrity is maintained by implementing rules such as one book per student, requiring checks for existing checkouts before allowing further transactions.
 
-#include <iostream>
-#include <string>
-#include <iomanip>
+#include <iostream> // for print
+#include <string>   // for strings
+#include <iomanip>  // for display manip
+#include <chrono>   // for time delay
+#include <thread>   // for time delay
 
 using namespace std;
 
 
+string validString(){
+    string name;
 
-int main() {    // display iomanip and call functions here
-    
+    while (true) {
+        getline(cin, name);
+
+        if (name.empty()) {
+            cout << "Invalid... Please enter your first name" << endl;
+        } else {
+            break;
+        }
+    }
+    return name;
+}
+
+
+int validInt(){
+    int num;
+    while (true){
+        cin >> num;
+        
+        if (cin.fail() || num <= 0){
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid... Please enter your 8-digit student ID" << endl;
+        } else {    // input is int and pos, now check if it is 8 digits or not
+            int digits = 0;
+            int numCopy = num;
+            while(numCopy > 0){
+                numCopy /= 10;
+                digits++;
+            }
+            if (digits == 8){
+                break;
+            }
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid... Please enter your 8-digit student ID" << endl;
+        }
+    }
+    return num;
+}
+
+
+void displayWelcome(){
     cout << setw(40) << setfill('=') << " " << setfill(' ') << endl;
     cout << "Hello, welcome to the virtual library!" << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    
+    cout << "What is your first name?" << endl;
+    string name = validString();
+    
+    cout << "What is your 8-digit student ID?" << endl;
+    int studentID = validInt();
+    
+    cout << setw(40) << setfill('=') << " " << setfill(' ') << endl;
+    cout << "Please confirm if this is your first name and SID: " << name << ", " << studentID << endl;
+
+}
+
+
+int main() {
+    displayWelcome();
 
     
     return 0;
